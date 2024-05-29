7e2d464f97bd7436d895ef359883e5636af1e1a8
JCMartinezUOC <jchicam@uoc.edu>
2024-05-29 20:16:54 +0200

    JS functional

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,6 +11,33 @@ import baguetteBox from 'baguettebox.js';
  * Write any other JavaScript below
  */
 
+if (document.title) {
+  switch (document.title) {
+    case "Chefs de pueblo: Los chefs":
+      // Header
+      document.querySelector('#nav-link-header-1').classList.add("text-navlink-active");
+      document.querySelector('#nav-link-header-2').classList.add("text-navlink","hover:text-navlink-hover");
+      document.querySelector('#nav-link-header-3').classList.add("text-navlink","hover:text-navlink-hover");
+      // Footer
+      document.querySelector('#nav-link-footer-1').classList.add("text-navlink-active");
+      document.querySelector('#nav-link-footer-2').classList.add("text-navlink","hover:text-navlink-hover");
+      document.querySelector('#nav-link-footer-3').classList.add("text-navlink","hover:text-navlink-hover");
+      break;
+    case "Chefs de pueblo: Galería":
+      // Header
+      document.querySelector('#nav-link-header-1').classList.add("text-navlink","hover:text-navlink-hover");
+      document.querySelector('#nav-link-header-2').classList.add("text-navlink-active");
+      document.querySelector('#nav-link-header-3').classList.add("text-navlink","hover:text-navlink-hover");
+      // Footer
+      document.querySelector('#nav-link-footer-1').classList.add("text-navlink","hover:text-navlink-hover");
+      document.querySelector('#nav-link-footer-2').classList.add("text-navlink-active");
+      document.querySelector('#nav-link-footer-3').classList.add("text-navlink","hover:text-navlink-hover");
+      break;
+    default:
+      break;
+  }
+}
+
 if (document.querySelector('.gallery') !== null) {
   baguetteBox.run('.gallery', {overlayBackgroundColor: 'oklch(40.1% 0.123 21.57 / 70%)'});
 }
